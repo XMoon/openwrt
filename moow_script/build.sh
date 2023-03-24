ca918408794b6ddf0fc6ee471a205c3bdf66912b
XMoon <xmoonox@gmail.com>
2023-03-24 17:15:54 +0800

    [moow_script] add build.sh

---
+++ moow_script/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ ! -f feeds.conf.default ]; then
+    echo "must run this script in the top of openwrt source dir"
+    exit 1
+fi
+
+set -e
+
+# clean first
+echo "step 1: clean old build & tmp"
+make clean
+rm -rf .tmp
+
+# feed update
+echo "step 2: reset & update feeds"
+
+./scripts/feeds update -af
+
+# rm smartdns from offical feed, use moow_package one
+if [ -d "$(pwd)/feeds/packages/net/smartdns" ];then
+    rm -rf "$(pwd)/feeds/packages/net/smartdns"
+fi
+
+if [ -d "$(pwd)/feeds/luci/applications/luci-app-smartdns" ];then
+    rm -rf "$(pwd)/feeds/luci/applications/luci-app-smartdns"
+fi
+
+./scripts/feeds update -ai
+./scripts/feeds install -af
+
+echo "step 3: build"
+cp -a makeconfig.d/x86_64.config .config
+make -j "$(nproc)" ||  make -j1 V=s
